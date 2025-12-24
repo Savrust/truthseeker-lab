2cdceb84e020c991c9edd3b81735486ee0db7ca7
Savrust <fusitashotta@gmail.com>
2025-12-24 12:39:05 +0900

    fix

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -22,12 +22,14 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between py-4 font-medium transition-all [&[data-state=open]>svg]:rotate-180",
         className,
       )}
       {...props}
     >
-      {children}
+      <span className="hover:scale-105 origin-left transition-transform duration-200 inline-block">
+        {children}
+      </span>
       <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
